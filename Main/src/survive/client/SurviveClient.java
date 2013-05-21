4e8de92be2535cf4577c7d6214804f47f13ee4e7
Andrey Kashin <iiotep9huy@gmail.com>
2013-05-21 23:57:26 +0400

    Project base structure created.

---
+++ Main/src/survive/client/SurviveClient.java
@@ -0,0 +1,17 @@
+package survive.client;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iiotep9huy
+ * Date: 5/21/13
+ * Time: 11:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SurviveClient extends Game {
+
+    @Override
+    public void create() {
+    }
+}
---
+++ Main/src/survive/server/SurviveServer.java
@@ -0,0 +1,14 @@
+package survive.server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iiotep9huy
+ * Date: 5/21/13
+ * Time: 11:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SurviveServer {
+
+    public void run() {
+    }
+}
